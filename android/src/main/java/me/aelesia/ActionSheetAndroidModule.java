18e949fdca4b83fa4f8338f9079c5ea30b5b6a1b
Mykyta Rusyn <nikrus1275@gmail.com>
2023-09-13 13:29:02 +0300

    init

--- android/src/main/java/me/aelesia/ActionSheetAndroidModule.java
+++ android/src/main/java/me/aelesia/ActionSheetAndroidModule.java
@@ -2,6 +2,8 @@ package me.aelesia;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -23,6 +25,8 @@ public class ActionSheetAndroidModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
     private boolean isShowingDialog = false;
+    private Handler mainHandler = new Handler(Looper.getMainLooper());
+
 
     public ActionSheetAndroidModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -37,84 +41,90 @@ public class ActionSheetAndroidModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void options(String title, String message, String cancel, ReadableArray option, int destructiveIndex, String tintColor, Promise promise) {
         if (!isShowingDialog) {
-            List<Object> messageStrList = option.toArrayList();
-            List<String> strList = new ArrayList<String>();
+            Runnable myRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    List<Object> messageStrList = option.toArrayList();
+                    List<String> strList = new ArrayList<String>();
 
-            for (Object msg: messageStrList) {
-                if (msg instanceof String) {
-                    strList.add((String) msg);
+                    for (Object msg: messageStrList) {
+                        if (msg instanceof String) {
+                            strList.add((String) msg);
+                        }
+                    }
+
+
+                    BottomSheetDialog dialog = new BottomSheetDialog(getCurrentActivity(), R.style.BottomSheetDialog);
+                    dialog.setContentView(R.layout.actionsheet);
+
+                    if (title != null || message != null) {
+                        LinearLayout header = dialog.findViewById(R.id.actionsheet_header);
+                        header.setPadding(0, 24, 0, 24);
+                        if (title != null) {
+                            TextView titleText = dialog.findViewById(R.id.actionsheet_title);
+                            titleText.setText(title);
+                            titleText.setVisibility(View.VISIBLE);
+                        }
+
+                        if (message != null) {
+                            TextView messageText = dialog.findViewById(R.id.actionsheet_message);
+                            messageText.setText(message);
+                            messageText.setPadding(0, 12, 0, 0);
+                            messageText.setVisibility(View.VISIBLE);
+                        }
+                    }
+
+                    if (cancel == null) {
+                        dialog.setCancelable(false);
+                    } else {
+                        dialog.setOnCancelListener(dialog1 -> {
+                            isShowingDialog = false;
+                            promise.resolve(-1);
+                        });
+                    }
+
+                    ListView listView = dialog.findViewById(R.id.actionsheet_list);
+                    if (title != null || message != null) {
+                        View border = new View(reactContext);
+                        border.setBackgroundColor(Color.parseColor("#DDDDDD"));
+                        border.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 1));
+                        listView.addHeaderView(border);
+                    }
+                    ActionSheetListAdapter adapter = new ActionSheetListAdapter(reactContext, strList, destructiveIndex, tintColor, position -> {
+                        dialog.dismiss();
+                        isShowingDialog = false;
+                        promise.resolve(position);
+                    });
+                    listView.setAdapter(adapter);
+
+                    if (cancel != null) {
+                        View border = new View(reactContext);
+                        border.setBackgroundColor(Color.parseColor("#DDDDDD"));
+                        border.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 6));
+                        listView.addFooterView(border);
+
+                        dialog.findViewById(R.id.actionsheet_list);
+                        TextView cancelText = dialog.findViewById(R.id.actionsheet_cancel);
+                        try {
+                            cancelText.setTextColor(Color.parseColor(tintColor));
+                        } catch(Exception e) {
+                            cancelText.setTextColor(Color.parseColor("#222222"));
+                        }
+                        cancelText.setText(cancel);
+                        LinearLayout cancelView = dialog.findViewById(R.id.actionsheet_cancel_view);
+                        cancelView.setVisibility(View.VISIBLE);
+                        cancelView.setOnClickListener(v -> {
+                            dialog.dismiss();
+                            isShowingDialog = false;
+                            promise.resolve(-1);
+                        });
+                    }
+
+                    isShowingDialog = true;
+                    dialog.show();
                 }
-            }
-
-
-            BottomSheetDialog dialog = new BottomSheetDialog(getCurrentActivity(), R.style.BottomSheetDialog);
-            dialog.setContentView(R.layout.actionsheet);
-
-            if (title != null || message != null) {
-                LinearLayout header = dialog.findViewById(R.id.actionsheet_header);
-                header.setPadding(0, 24, 0, 24);
-                if (title != null) {
-                    TextView titleText = dialog.findViewById(R.id.actionsheet_title);
-                    titleText.setText(title);
-                    titleText.setVisibility(View.VISIBLE);
-                }
-
-                if (message != null) {
-                    TextView messageText = dialog.findViewById(R.id.actionsheet_message);
-                    messageText.setText(message);
-                    messageText.setPadding(0, 12, 0, 0);
-                    messageText.setVisibility(View.VISIBLE);
-                }
-            }
-
-            if (cancel == null) {
-                dialog.setCancelable(false);
-            } else {
-                dialog.setOnCancelListener(dialog1 -> {
-                    isShowingDialog = false;
-                    promise.resolve(-1);
-                });
-            }
-
-            ListView listView = dialog.findViewById(R.id.actionsheet_list);
-            if (title != null || message != null) {
-                View border = new View(reactContext);
-                border.setBackgroundColor(Color.parseColor("#DDDDDD"));
-                border.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 1));
-                listView.addHeaderView(border);
-            }
-            ActionSheetListAdapter adapter = new ActionSheetListAdapter(reactContext, strList, destructiveIndex, tintColor, position -> {
-                dialog.dismiss();
-                isShowingDialog = false;
-                promise.resolve(position);
-            });
-            listView.setAdapter(adapter);
-
-            if (cancel != null) {
-                View border = new View(reactContext);
-                border.setBackgroundColor(Color.parseColor("#DDDDDD"));
-                border.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 6));
-                listView.addFooterView(border);
-
-                dialog.findViewById(R.id.actionsheet_list);
-                TextView cancelText = dialog.findViewById(R.id.actionsheet_cancel);
-                try {
-                    cancelText.setTextColor(Color.parseColor(tintColor));
-                } catch(Exception e) {
-                    cancelText.setTextColor(Color.parseColor("#222222"));
-                }
-                cancelText.setText(cancel);
-                LinearLayout cancelView = dialog.findViewById(R.id.actionsheet_cancel_view);
-                cancelView.setVisibility(View.VISIBLE);
-                cancelView.setOnClickListener(v -> {
-                    dialog.dismiss();
-                    isShowingDialog = false;
-                    promise.resolve(-1);
-                });
-            }
-
-            isShowingDialog = true;
-            dialog.show();
+            };
+            mainHandler.post(myRunnable);
         }
     }
 }
--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-export { ActionSheet, ActionSheetConfig, ActionSheetOption } from './src/ActionSheet'
-export { ActionSheetAndroid, ActionSheetAndroidOptions } from './src/ActionSheetAndroid'
+export { ActionSheet, type ActionSheetConfig, type ActionSheetOption } from './src/ActionSheet'
+export { ActionSheetAndroid, type ActionSheetAndroidOptions } from './src/ActionSheetAndroid'
 export { ActionSheetCancelledError } from './src/ActionSheetCancelledError'
